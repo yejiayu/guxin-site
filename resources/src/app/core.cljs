323fcb3969d09cfb18077ebe567858b59aa9a5e8
yejiayu <yejiayu.fe@gmail.com>
2016-04-09 21:22:13 +0800

    【Chore】add bootstrap css

--- resources/src/app/core.cljs
+++ resources/src/app/core.cljs
@@ -1,6 +1,8 @@
 (ns app.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [om-bootstrap.button :as b]
+            [om-tools.dom :as d :include-macros true]))
 
 (enable-console-print!)
 
@@ -8,14 +10,19 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello Clojure!"}))
+;; (defonce app-state (atom {:text "Hello Clojure!"}))
+
+(defn Index [data onwer]
+  (reify
+    om/IRender
+    (render [_]
+      (d/div
+        (b/button {:bs-style "primary"} "我是一个按钮")
+        (dom/h1 nil (:text data))))))
 
 (om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
+  Index
+  {:text "Hello Clojure!"}
   {:target (. js/document (getElementById "app"))})
 
 
